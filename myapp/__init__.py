d27f6f82868a4bc953d6db58390410bb004a004a
DanielK <rafikrafikrafik@yandex.ru>
2020-09-23 20:40:41 +0300

    Add Redis channel to subscribe to result of redis worker; Create class to make kinda singleton of redis PubSub so it can be shared across Flask handlers not using session(its not great but it works for this pet project)

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,3 +1,5 @@
+import json
+
 from celery import Celery
 from flask import Flask, current_app, url_for, redirect, render_template, _app_ctx_stack
 import redis
@@ -6,7 +8,7 @@ import pika
 from myapp.celery_utils import celery_app, init_celery
 from myapp.config import Config
 from workers.celery_worker import write_task
-from workers import celery_worker
+from workers import celery_worker, redis_pubsub_worker
 
 
 
@@ -50,16 +52,42 @@ def create_app():
     @app.route("/redis")
     def redis_view():
         r = redis.Redis(host="redis", port=6379)
+        #r = redis.Redis(host="localhost", port=6379)
         r.mset({"Croatia": "Zagreb", "Bahamas": "Nassau"})
         # True
         result = r.get("Bahamas")
         # b'Nassau'
+
+        # Subscribe to result_channel b4 send task so rsult can be caught
+        c = redis_pubsub_worker.RedisUniquePubSub(r)
+        p = c.subscribe_to_result_once()
+        print(f'In send - {p}')
+
+
 
         r.publish('my-first-channel', '1 Cool message to publish!')
         r.publish('my-first-channel', '2 Cool message to publish!')
         r.publish('my-first-channel', 'And even 3 Cool message to publish!')
 
         return f'Hey redis - {result}', 200
+
+    @app.route('/redis_result')
+    def redis_check_result():
+        r = redis.Redis(host="redis", port=6379)
+
+        c = redis_pubsub_worker.RedisUniquePubSub(r)
+        p = c.subscribe_to_result_once()
+        print(f'In result - {p}')
+
+
+        result_message = p.get_message()
+        if result_message:
+            print(f"{result_message['data']}")
+            result_dict = json.loads(result_message['data']) if result_message['data']!=1 else {'message':'some_other'}
+            return {'status':'Finished', 'message' : result_dict['message']}, 200
+        else:
+            return {'status':'Pending'}, 200
+
 
     @app.route("/celery")
     def celery_view():
@@ -71,4 +99,4 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(port=5000, debug=True)
+    app.run(port=5555, debug=True)
--- workers/redis_pubsub_worker.py
+++ workers/redis_pubsub_worker.py
@@ -1,15 +1,48 @@
 import datetime
+import json
 
 import redis
 
 
+#def subscribe_to_result_once(r):
 
-r = redis.Redis(host="redis", port=6379)
+class RedisUniquePubSub():
 
-p = r.pubsub()
-p.subscribe('my-first-channel')
+    pubsub = None
 
-for message in p.listen():
-    print(f"Got message! Its content - {message['data']}")# do something with the message
-    with open("temp.txt", "a") as file:
-        file.write(f"{datetime.datetime.now()}, {message['data']} \n")
+    def __init__(self, r):
+        self.r=r
+
+    def subscribe_to_result_once(self):
+        if not RedisUniquePubSub.pubsub:
+            print('Called SUBSCRIBE!!!')
+            RedisUniquePubSub.pubsub = self.r.pubsub()
+            RedisUniquePubSub.pubsub.subscribe('result_channel')
+        else:
+            pass
+
+        return RedisUniquePubSub.pubsub
+
+
+def run_worker():
+
+    r = redis.Redis(host="redis", port=6379)
+    #r = redis.Redis(host="localhost", port=6379)
+
+    p = r.pubsub()
+    p.subscribe('my-first-channel')
+
+    for message in p.listen():
+        print(f"Got message! Its content - {message['data']}")# do something with the message
+        #with open("temp.txt", "a") as file:
+        #    file.write(f"{datetime.datetime.now()}, {message['data']} \n")
+        
+        message_str = message['data'].decode() if message['data']!=1 else "some_string"
+
+        result_dict = {'finished':True, 'message':message_str}
+        result_json = json.dumps(result_dict)
+        print('Publishing to result_channel...')
+        r.publish('result_channel', result_json)
+
+if __name__ == '__main__':
+    run_worker()

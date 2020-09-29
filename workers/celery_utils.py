815b92d0eef64f0ee4ca53abb483844016bac78d
DanielK <rafikrafikrafik@yandex.ru>
2020-09-29 18:39:09 +0300

    For some reason i forgot to add celery_worker file

---
+++ workers/celery_utils.py
@@ -0,0 +1,22 @@
+from celery import Celery
+
+
+def make_celery(app_name=__name__):
+    celery_app = Celery(app_name, broker="pyamqp://rabbit:mq@rabbit:5672")
+    # celery_app = Celery(app_name, broker="pyamqp://guest:guest@localhost:5672")
+    # celery.conf.update(app.config)
+    return celery_app
+
+
+def init_celery(celery_app, app):
+    celery_app.conf.update(app.config)
+    TaskBase = celery_app.Task
+
+    class ContextTask(TaskBase):
+        testcheck = "CtxTask"
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+
+    celery_app.Task = ContextTask

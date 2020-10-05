937980519c69fe19b7bb52b30ded8382d237b77e
DanielK <rafikrafikrafik@yandex.ru>
2020-10-05 15:05:31 +0300

    Move rabbit worker functonality into a function so it can be imported and called separately

--- workers/rabbit_worker.py
+++ workers/rabbit_worker.py
@@ -2,35 +2,47 @@ import time
 import datetime
 import pika
 
-# Sleep for 20 sec to ensure that rabbit server started
-print(' [*] Sleeping for 120 seconds.')
-time.sleep(120)
-print(' [*] After sleep')
-print(' [*] Connecting to server ...')
+def run_worker():
+    # Sleep for 20 sec to ensure that rabbit server started
+    print(' [*] Sleeping for 120 seconds.')
+    time.sleep(120)
+    print(' [*] After sleep')
+    print(' [*] Connecting to server ...')
 
-##def start_consuming():
-credentials = pika.PlainCredentials('rabbit', 'mq')
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='rabbit', credentials = credentials))
-channel = connection.channel()
-
-channel.queue_declare(queue='hello', durable=True)
-
-
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
-    with open('temp.txt', 'a') as file:
-        file.write(f"{datetime.datetime.now()} \n")
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+    ##def start_consuming():
+    #credentials = pika.PlainCredentials('rabbit', 'mq')
+    #connection = pika.BlockingConnection(
+    #    pika.ConnectionParameters(host='rabbit', credentials = credentials))
+    
+    credentials = pika.PlainCredentials('guest', 'guest')
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost', credentials = credentials))
 
 
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(
-    queue='hello', on_message_callback=callback)#, auto_ack=True)
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel = connection.channel()
 
-channel.start_consuming()
 
-print("End of worker")
+    channel.queue_declare(queue='hello', durable=True)
+
+
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+        with open('temp.txt', 'a') as file:
+            file.write(f"{datetime.datetime.now()} \n")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(
+        queue='hello', on_message_callback=callback)#, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+
+    channel.start_consuming()
+
+    print("End of worker")
+
+if __name__ == "__main__":
+    run_worker()
 

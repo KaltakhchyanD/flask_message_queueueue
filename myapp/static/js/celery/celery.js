840abe8e1c41bbeabdfc48c1de7a286eb7d9c65b
DanielK <rafikrafikrafik@yandex.ru>
2020-11-25 08:13:37 +0300

    Add ability to receive and show status of each started task at celery.js

--- myapp/static/js/celery/celery.js
+++ myapp/static/js/celery/celery.js
@@ -15,7 +15,26 @@ class Model {
         let data = await response.text();
         return data;
     }
+
+    async check_result(message_json) {
+        let options = {
+            method: "POST",
+            cache: "no-cache",
+            headers: {
+                "Content-Type": "application/json",
+                "accepts": "application/json"
+            },
+            body: JSON.stringify(message_json)
+        };
+        // Call the REST endpoint and wait for data
+        let endpoint_url = "/celery_check_result";
+        let response = await fetch(endpoint_url, options);
+        let data = await response.json();
+        return data;
+    }
+
 }
+
 
 class View {
     constructor(){
@@ -30,10 +49,34 @@ class View {
         data_html+="<br>"
         data_html+= "</div>"
 
-        let old_html = $("#task_created").children("#task_created").html();
-        $("#task_created").children("#task_created").html(old_html+data_html);
+        let old_html = $("#task_created").html();
+        $("#task_created").html(old_html+data_html);
     }
+
+    show_pending(task_id){
+        let data_html = "<div class=\"task_pending\" id="+task_id+">";
+        data_html+="<br>"
+        data_html+= "<h3>Task status: Pending</h3>" 
+        data_html+="<br>"
+        data_html+= "</div>"
+
+        $("#"+task_id).html(data_html);
+    }
+
+    show_task_is_ready(task_id, message){
+        //console.log("IN show ready with "+task_id+" and "+message);
+        let data_html = "<div class=\"task_pending\" id="+task_id+">";
+        data_html+="<br>"
+        data_html+= "<h3>Task status: Finnish</h3>" 
+        data_html+= "<h3>Message: "+message+"</h3>" 
+        data_html+="<br>"
+        data_html+= "</div>"
+
+        $("#"+task_id).html(data_html);
+    }
+
 }
+
 
 function uuidv4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
@@ -41,6 +84,10 @@ function uuidv4() {
     return v.toString(16);
   });
 }
+
+
+let pending_tasks_id = []
+
 
 class Controller{
     constructor(model, view) {
@@ -51,26 +98,61 @@ class Controller{
 
     async initialize() {
         this.initializeButtonEvent();
-        //this.initializeCheckResultButtonEvent();
+        this.initializeCheckResultButtonEvent();
     }
 
     async start_task_button_action(evt, button){
         evt.preventDefault();
         try {
             let task_id = uuidv4();
-            // !!!
             let create_json = {
                 "message":$("#messageInput").val(),
                 "task_id":task_id,
             }
             let response = await this.model.start_task(create_json);
-
+            pending_tasks_id.push(task_id);
             console.log(response)
             this.view.show_task_been_created(task_id);
         } catch(err) {
             console.log(err)
         }
     }
+
+    async check_result_button_action(evt, button){
+        evt.preventDefault();
+        try {
+
+            // Make all tasks pending and show it
+            for (let task_id of pending_tasks_id){
+                this.view.show_pending(task_id);
+            }
+
+            // Iterate over pending tasks list
+            // If its ready - show it
+            // then increment counter
+            let ready_tasks = 0;
+            for (let task_id of pending_tasks_id){
+                let check_result_json = {'task_id':task_id};
+                let response = await this.model.check_result(check_result_json);
+
+                if (response["status"]==="Ready"){
+                    console.log("CMOOON");
+                    this.view.show_task_is_ready(task_id, response["message"]);
+                    ready_tasks++;
+                }
+            }
+
+            // Remove first n tasks from pending tasks list
+            // where n == num of tasks ready
+            for(let i=0; i<ready_tasks; i++){
+                pending_tasks_id.shift();
+            }
+
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
 
     initializeButtonEvent(){
         let send_buttons = $( ":button[name^='StartTask']" );
@@ -79,8 +161,14 @@ class Controller{
         }
     }
 
-}
+    initializeCheckResultButtonEvent(){
+        let send_buttons = $( ":button[name^='CheckResult']" );
+        for (const some_button of send_buttons){
+            some_button.addEventListener ("click", (evt) =>this.check_result_button_action(evt, some_button));
+        }
+    }
 
+}
 
 
 

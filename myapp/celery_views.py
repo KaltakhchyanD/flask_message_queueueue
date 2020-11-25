ff8bb1c2aea0f67847bc3708e1aa08619acce731
DanielK <rafikrafikrafik@yandex.ru>
2020-11-25 08:12:54 +0300

    Add handler to celery_views.py that gets POST with task_id, checks its status, returns status and message, if tasks is ready

--- myapp/celery_views.py
+++ myapp/celery_views.py
@@ -4,6 +4,8 @@ from workers.celery_worker import write_task, dummy_task
 
 blueprint = Blueprint("celery", __name__, url_prefix="")
 
+task_id_async_result_dict = {}
+
 
 @blueprint.route("/celery")
 def celery_view():
@@ -26,6 +28,38 @@ def run_task():
     if not task_json["task_id"]:
         return {"error_code": 400, "error_message": "task_id should not be empty"}, 400
 
-    dummy_task.delay(task_json)
+    async_result = dummy_task.delay(task_json)
+    task_id_async_result_dict[task_json["task_id"]] = async_result
+
+
     print("Started celery task")
     return "<h1>Started celery task</h1>"
+
+@blueprint.route('/celery_check_result', methods = ["POST"])
+def check_task_result():
+    task_json = request.get_json()
+
+    if "task_id" not in task_json.keys():
+        return (
+            {
+                "error_code": 400,
+                "error_message": "task_json should contain 'task_id' ",
+            },
+            400,
+        )
+
+    if not task_json["task_id"]:
+        return {"error_code": 400, "error_message": "task_id should not be empty"}, 400
+
+    async_result_to_check = task_id_async_result_dict[task_json["task_id"]]
+
+    if async_result_to_check.ready():
+        message=async_result_to_check.get()
+        result_json = {'status':"Ready", 'message': message}
+    else:
+        result_json = {'status':"Pending", 'message': 'garbage'}
+
+
+    return result_json, 200
+
+

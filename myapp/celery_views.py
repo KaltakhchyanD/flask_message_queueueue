56e10a899a9dc0165075a4db4961bfafcdcd4972
DanielK <rafikrafikrafik@yandex.ru>
2021-01-09 19:37:13 +0300

    Add priority and quantity parameters as expected ones to receive at celery_create_task handler, add validation of those parameters

--- myapp/celery_views.py
+++ myapp/celery_views.py
@@ -9,21 +9,41 @@ task_id_async_result_dict = {}
 
 @blueprint.route("/celery")
 def celery_view():
-    return render_template('celery_page.html')
+    return render_template("celery_page.html")
 
 
 @blueprint.route("/celery_create_task", methods=["POST"])
 def run_task():
     task_json = request.get_json()
 
-    if "message" not in task_json.keys() and "task_id" not in task_json.keys():
+    if (
+        "message" not in task_json.keys()
+        or "quantity" not in task_json.keys()
+        or "priority" not in task_json.keys()
+        or "task_id" not in task_json.keys()
+    ):
         return (
             {
                 "error_code": 400,
-                "error_message": "task_json should contain 'message' and 'task_id' ",
+                "error_message": "task_json should contain 'message', 'quantity', 'priority' and 'task_id' ",
             },
             400,
         )
+
+    if not task_json["quantity"]:
+        return {"error_code": 400, "error_message": "quantity should not be empty"}, 400
+    elif not task_json["quantity"].isdigit():
+        return {"error_code": 400, "error_message": "quantity should be digit"}, 400
+    elif not int(task_json["quantity"])>0:
+        return {"error_code": 400, "error_message": "quantity should more then 0"}, 400
+    
+
+    if not task_json["priority"]:
+        return {"error_code": 400, "error_message": "priority should not be empty"}, 400
+    elif not task_json["priority"].isdigit():
+        return {"error_code": 400, "error_message": "priority should be digit"}, 400
+    elif not 0<int(task_json["priority"])<4:
+        return {"error_code": 400, "error_message": "priority should between 1 and 3"}, 400
 
     if not task_json["task_id"]:
         return {"error_code": 400, "error_message": "task_id should not be empty"}, 400
@@ -31,20 +51,17 @@ def run_task():
     async_result = dummy_task.delay(task_json)
     task_id_async_result_dict[task_json["task_id"]] = async_result
 
-
     print("Started celery task")
     return "<h1>Started celery task</h1>"
 
-@blueprint.route('/celery_check_result', methods = ["POST"])
+
+@blueprint.route("/celery_check_result", methods=["POST"])
 def check_task_result():
     task_json = request.get_json()
 
     if "task_id" not in task_json.keys():
         return (
-            {
-                "error_code": 400,
-                "error_message": "task_json should contain 'task_id' ",
-            },
+            {"error_code": 400, "error_message": "task_json should contain 'task_id' "},
             400,
         )
 
@@ -54,12 +71,9 @@ def check_task_result():
     async_result_to_check = task_id_async_result_dict[task_json["task_id"]]
 
     if async_result_to_check.ready():
-        message=async_result_to_check.get()
-        result_json = {'status':"Ready", 'message': message}
+        message = async_result_to_check.get()
+        result_json = {"status": "Ready", "message": message}
     else:
-        result_json = {'status':"Pending", 'message': 'garbage'}
-
+        result_json = {"status": "Pending", "message": "garbage"}
 
     return result_json, 200
-
-

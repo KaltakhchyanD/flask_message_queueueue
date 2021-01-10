98955b5dbb5c4c8bcf5f5adb0e4d16abc7adea0e
DanielK <rafikrafikrafik@yandex.ru>
2021-01-10 20:38:14 +0300

    Update error codes for errors at /celery_create_task handler - now there are distinct codes for different errors, which are sent to front end inside json

--- myapp/celery_views.py
+++ myapp/celery_views.py
@@ -24,29 +24,29 @@ def run_task():
     ):
         return (
             {
-                "error_code": 400,
+                "error_code": 1,
                 "error_message": "task_json should contain 'message', 'quantity', 'priority' and 'task_id' ",
             },
             400,
         )
 
     if not task_json["quantity"]:
-        return {"error_code": 400, "error_message": "quantity should not be empty"}, 400
+        return {"error_code": 2, "error_message": "quantity should not be empty"}, 400
     elif not task_json["quantity"].isdigit():
-        return {"error_code": 400, "error_message": "quantity should be digit"}, 400
+        return {"error_code": 3, "error_message": "quantity should be digits"}, 400
     elif not int(task_json["quantity"])>0:
-        return {"error_code": 400, "error_message": "quantity should more then 0"}, 400
+        return {"error_code": 4, "error_message": "quantity should more then 0"}, 400
     
 
     if not task_json["priority"]:
-        return {"error_code": 400, "error_message": "priority should not be empty"}, 400
+        return {"error_code": 5, "error_message": "priority should not be empty"}, 400
     elif not task_json["priority"].isdigit():
-        return {"error_code": 400, "error_message": "priority should be digit"}, 400
+        return {"error_code": 6, "error_message": "priority should be digit"}, 400
     elif not 0<int(task_json["priority"])<4:
-        return {"error_code": 400, "error_message": "priority should between 1 and 3"}, 400
+        return {"error_code": 7, "error_message": "priority should between 1 and 3"}, 400
 
     if not task_json["task_id"]:
-        return {"error_code": 400, "error_message": "task_id should not be empty"}, 400
+        return {"error_code": 8, "error_message": "task_id should not be empty"}, 400
 
     async_result = dummy_task.delay(task_json)
     task_id_async_result_dict[task_json["task_id"]] = async_result
